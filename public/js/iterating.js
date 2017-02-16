d7eb2b6e46fe0cfd3e7f5a62b18468058ecb1059
John Nichols <jtn0113@yahoo.com>
2017-02-16 09:55:12 -0600

    added iterating exercise

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -6,11 +6,13 @@
     // TODO: Create a log statement that will log the number of elements in the names array.
     console.log(names.length);
     // TODO: Create log statements that will print each of the names array elements individually.
-    console.log('The first name is : ' + names[0]);
-    console.log('The second name is : ' + names[1]);
-    console.log('The third name is : ' + names[2]);
-    console.log('The fourth name is : ' + names[3]);
+    for (var i = 0; i < names.length; i++) {
+    	console.log(names[i]);
+    }
 
+    names.forEach(function (element, index, array) {
+    	console.log(names[index]);
+    })
 
 
 })();
