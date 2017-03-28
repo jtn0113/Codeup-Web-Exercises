e31b6c040f52b13ebeec2c9e0a9da2e05f30315c
John Nichols <jtn0113@yahoo.com>
2017-03-28 14:18:11 -0500

    added sessions exercise

--- public/login.php
+++ public/login.php
@@ -2,8 +2,16 @@
 
 function pageController() {
 
+	session_start();
+
+	$sessionId = session_id();
+
 	$data = [];
 	$data['notAuthorized'] = '';
+
+	if (isset($_SESSION['logged_in_user']) && $_SESSION['logged_in_user'] == 'guest') {
+		header("Location: authorized.php");
+	}
 
 	if (isset($_POST['username'])) {
 		$data['username'] = $_POST['username'];
@@ -14,6 +22,7 @@ function pageController() {
 	}
 
 	if (isset($data['username']) == 'guest' && $data['password'] == 'password') {
+		$_SESSION['logged_in_user'] = 'guest';
 		header("Location: authorized.php");
 	} else if (isset($data['username'])) {
 		$data['notAuthorized'] = "Not authorized";
@@ -24,6 +33,7 @@ function pageController() {
 
 extract(pageController());
 
+
 ?>
 
 <!DOCTYPE html>
@@ -31,15 +41,34 @@ extract(pageController());
 <head>
 	<title>POST Login</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-	 <form class"form-inline" action="login.php" method="POST">
+<!-- 	 <form class"form-inline" action="login.php" method="POST">
 	 	<label>Username</label>
 	 	<input class="input-xs" type="text" name="username">
 	 	<label>Password</label>
 	 	<input class="input-xs" type="password" name="password">
 	 	<button class="btn btn-xs btn-danger" type="submit">Login</button>
-	</form>
+	</form> -->
 	<h1> <?= $notAuthorized ?> </h1>
+
+
+
+	<div class="container">
+        <div class="card card-container">
+        <h2 class='login_title text-center'>Login</h2>
+        <hr>
+            <form class="form-signin" action="login.php" method="POST">
+                <span id="reauth-email" class="reauth-email"></span>
+                <p class="input_title"><span class="glyphicon glyphicon-user"></span></p>
+                <input type="text" id="inputEmail" class="login_box" placeholder="Username" name="username" required autofocus>
+                <p class="input_title"><span class="glyphicon glyphicon-lock"></span></p>
+                <input type="password" id="inputPassword" class="login_box" placeholder="Password" name="password" required>
+                <br><br>
+                <button class="btn btn-lg btn-primary" type="submit">Login</button>
+            </form><!-- /form -->
+        </div><!-- /card-container -->
+    </div><!-- /container -->
 </body>
 </html>
--- public/logout.php
+++ public/logout.php
@@ -2,20 +2,24 @@
 
 session_start();
 
-if (!isset($_SESSION['logged_in_user']) || $_SESSION['logged_in_user'] != 'guest') {
-	header("Location: login.php");
+function clearSession() {
+    session_unset();
+    session_regenerate_id(true);
 }
 
- ?>
+
+clearSession();
+
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Authorized</title>
+	<title>Logout</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-	<h4>Authorized</h4>
-	<a href="/logout.php"><span class="glyphicon glyphicon-log-out" style="color:red;font-size:50px"></span></a>
+	<h4>You have been logged out</h4>
+	<a href="/login.php"><span class="glyphicon glyphicon-log-in" style="color:green;font-size:50px"></span></a>
 </body>
 </html>
