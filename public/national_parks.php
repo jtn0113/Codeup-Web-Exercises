a1aa68c52c342e0a4c932453834be79c80804a26
John Nichols <jtn0113@yahoo.com>
2017-04-11 11:36:35 -0500

    added finished national parks exercise

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,13 +1,54 @@
 <?php
 
-require 'db_connect.php';
+require_once 'db_connect.php';
+require_once "../Input.php";
 
-$select = 'select * from national_parks';
+function pageController($dbc) {
 
-$statement = $dbc->query($select);
+	$data = [];
 
-$parks = $statement->fetchAll(PDO::FETCH_ASSOC);
+	$data['offset'] = 0;
+	
+	$data['page'] = 1;
 
+	if(Input::has('page')) {
+		Input::get('page');
+		$data['page'] = $_REQUEST['page'];
+		// Limits page from going too low
+		if($data['page'] < 0) {
+			$data['page'] = 0;
+		}
+		// Limits page from going too high
+		$sql = 'select count(*) from national_parks';
+		$statement = $dbc->query($sql);
+		$parksCount = $statement->fetchColumn();
+		$numberOfPages = ceil($parksCount/4);
+		if($data['page'] >= $numberOfPages) {
+			$data['page'] = $numberOfPages - 1;
+		}
+		$data['offset'] = $data['page'] * 4;
+	} else {
+		$data['page'] = 0;
+	}
+
+	if (Input::has('q')) {
+		if ($_REQUEST['q'] == 'next') {
+			$data['page'] + 1;
+		} else if ($_REQUEST['q'] == 'previous') {
+			$data['page'] - 1;
+		}
+	}
+	
+	$data['select'] = "select * from national_parks limit 4 offset " . $data['offset'];
+	
+	$data['statement'] = $dbc->query($data['select']);
+
+	$data['parks'] = $data['statement']->fetchAll(PDO::FETCH_ASSOC);
+
+	return $data;
+}
+
+extract(pageController($dbc));
 ?>
 
 <!DOCTYPE html>
@@ -34,5 +75,15 @@ $parks = $statement->fetchAll(PDO::FETCH_ASSOC);
 			</tr>
 		<?php endforeach; ?>
 	</table>
+ 	<form action="national_parks.php" method="get">
+	 	<input type="hidden" name="q" value="previous">
+	 	<input type="hidden" name="page" value=<?= $page-1 ?>>
+	 	<button class="btn btn-danger" type="submit">Previous</button>
+	</form>
+	<form action="national_parks.php" method="get">
+	 	<input type="hidden" name="q" value="next">
+	 	<input type="hidden" name="page" value=<?= $page+1 ?>>
+	 	<button class="btn btn-danger" type="submit">Next</button>
+	</form>
 </body>
 </html>
